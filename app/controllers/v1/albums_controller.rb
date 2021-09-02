3221e7b014a6d2de50f4c0ce24c33d4441e2abd5
Raymond <raymond@philippinedev.com>
2021-09-03 04:18:41 +0800

    album create

--- app/controllers/v1/albums_controller.rb
+++ app/controllers/v1/albums_controller.rb
@@ -9,5 +9,15 @@ module V1
                     .includes(:photos)
                     .first
     end
+
+    def create
+      album = Album.new(name: params[:name])
+
+      if album.save
+        render json: { status: 'success' }
+      else
+        render json: { status: 'failed' }, status: :unprocessable_entity
+      end
+    end
   end
 end
--- spec/requests/v1/albums_spec.rb
+++ spec/requests/v1/albums_spec.rb
@@ -19,36 +19,65 @@ RSpec.describe V1::AlbumsController do
     }
   end
 
-  before do
-    albums.each do |album|
-      FactoryBot.create_list(:photo, 3, album: album)
+  context 'for viewing' do
+    before do
+      albums.each do |album|
+        FactoryBot.create_list(:photo, 3, album: album)
+      end
+    end
+
+    describe 'GET /albums' do
+      let!(:albums) { FactoryBot.create_list(:album, 3) }
+      let(:expected_body) { albums.as_json(json_format) }
+
+      subject { get '/v1/albums' }
+
+      it 'returns a list of albums with photos' do
+        subject
+        expect(response.parsed_body).to include_json(expected_body)
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'GET /albums/:id' do
+      let!(:album) { FactoryBot.create(:album) }
+      let(:albums) { [album] }
+      let(:expected_body) { album.as_json(json_format) }
+
+      subject { get "/v1/albums/#{album.id}" }
+
+      it 'returns an album with photos' do
+        subject
+        expect(response.parsed_body).to include_json(expected_body)
+        expect(response.status).to eq(200)
+      end
     end
   end
 
-  describe 'GET /albums' do
-    let!(:albums) { FactoryBot.create_list(:album, 3) }
-    let(:expected_body) { albums.as_json(json_format) }
+  describe 'POST /albums' do
+    let(:name) { nil }
+    subject { post "/v1/albums", params: { name: name } }
 
-    subject { get '/v1/albums' }
+    context 'with valid name' do
+      let(:name) { 'Albumi' }
 
-    it 'returns a list of albums with photos' do
-      subject
-      expect(response.parsed_body).to include_json(expected_body)
-      expect(response.status).to eq(200)
+      it 'returns ok' do
+        subject
+        expect(response.parsed_body).to eq('status' => 'success')
+        expect(response.status).to eq(200)
+      end
+
+      it 'creates one album' do
+        expect { subject }.to change { Album.where(name: name).count }.by(1)
+      end
     end
-  end
 
-  describe 'GET /albums/:id' do
-    let!(:album) { FactoryBot.create(:album) }
-    let(:albums) { [album] }
-    let(:expected_body) { album.as_json(json_format) }
-
-    subject { get "/v1/albums/#{album.id}" }
-
-    it 'returns an album with photos' do
-      subject
-      expect(response.parsed_body).to include_json(expected_body)
-      expect(response.status).to eq(200)
+    context 'with invalid name' do
+      it 'will fail' do
+        subject
+        expect(response.parsed_body).to eq('status' => 'failed')
+        expect(response.status).to eq(422)
+      end
     end
   end
 end
