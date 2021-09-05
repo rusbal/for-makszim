f435244bca85e7d45d20ee6e7afdf57bbb288566
Raymond <raymond@philippinedev.com>
2021-09-05 13:07:57 +0800

    removed old code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   devise_for :users
-  get 'photos/show'
 
   namespace :v1, { defaults: { format: :json } } do
     resources :albums
