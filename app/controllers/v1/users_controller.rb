9f171edc55d921e98b6bf5e050e1b13ec48af0f1
Raymond <raymond@philippinedev.com>
2021-09-05 11:50:24 +0800

    added users controller

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,51 @@
+module V1
+  class UsersController < ApplicationController
+    def index
+      @users = User.all
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def create
+      user = User.new(user_params)
+
+      if user.save
+        success
+      else
+        failure
+      end
+    end
+
+    def update
+      user = User.find(params[:id])
+
+      if user.update(user_params)
+        success
+      else
+        failure
+      end
+    rescue ActiveRecord::RecordNotFound
+      failure
+    end
+
+    def destroy
+      user = User.find(params[:id])
+
+      if user.delete
+        success
+      else
+        failure
+      end
+    rescue ActiveRecord::RecordNotFound
+      failure
+    end
+
+    private
+
+    def user_params
+      params.permit(:first_name, :last_name)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   devise_for :users
   get 'photos/show'
 
   namespace :v1, { defaults: { format: :json } } do
     resources :albums
     resources :photos
+    resources :users
   end
 end
