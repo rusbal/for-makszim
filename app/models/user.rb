50313dad20bd27e3131408e47dbb1a1ee90c3376
Raymond <raymond@philippinedev.com>
2021-09-05 11:50:32 +0800

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  alias_method :authenticate, :valid_password?
+
+  def from_token_payload(payload)
+    self.find payload['sub']
+  end
 end
