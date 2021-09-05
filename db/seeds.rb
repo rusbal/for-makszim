c1bcc8b17667402b0b49bfa37d8501c4e63cf9c2
Raymond <raymond@philippinedev.com>
2021-09-05 11:46:35 +0800

    factory and seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_users
+  truncate('users')
+  15.times { FactoryBot.create(:user) }
+  p "Created #{User.count} users."
+end
+
+def truncate(table_name)
+  ActiveRecord::Base.connection.execute("TRUNCATE #{table_name} RESTART IDENTITY")
+end
+
+create_users
