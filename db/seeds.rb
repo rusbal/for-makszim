89f24e511fe5a7fb4e24ea631e279c3ed3db693c
Raymond <raymond@philippinedev.com>
2021-09-05 13:07:19 +0800

    seeder for tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
 def create_users
-  truncate('users')
+  User.destroy_all
   15.times { FactoryBot.create(:user) }
   p "Created #{User.count} users."
 end
 
-def truncate(table_name)
-  ActiveRecord::Base.connection.execute("TRUNCATE #{table_name} RESTART IDENTITY")
+def create_albums_and_photos
+  Photo.destroy_all
+  Album.destroy_all
+
+  50.times do
+    album = FactoryBot.create(:album)
+    [1, 2, 3, 4, 5].sample.times do
+      FactoryBot.create(:photo, album: album)
+    end
+  end
+
+  p "Created #{Album.count} albums."
+  p "Created #{Photo.count} photos."
 end
 
 create_users
+create_albums_and_photos
